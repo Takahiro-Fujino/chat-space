f5e722422ad608906cc77f9b604087830aae51f3
Takahiro-Fujino <fujinon24@fujinon24noMacBook-Pro.local>
2019-02-04 11:40:25 +0900

    Fixed_Message_index_1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,8 @@
 class MessagesController < ApplicationController
-
   def index
   end
 
   def create
     @message = Message.new
   end
-
-
-# ここから下は未実装
-  #   def new
-  #   @message = message.new
-  # end
-
-  # def create
-  #   Review.create(create_params)
-  #   render action: :new #表示したいビューを表示
-  # end
-
-  # private
-  # def create_params
-  #   params.require(:review).permit(:text)
-  # end
-
 end
