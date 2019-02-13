7307304c1d5a9f4ba958003ecae8606be673d18e
Takahiro-Fujino <fujinon24@fujinon24noMacBook-Pro.local>
2019-02-13 14:23:07 +0900

    Add_Ajax_1

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(function(){
+  function buildHTML(message){
+    if(message.image !== null)
+       var image = `<img class="lower-message__image" src="${message.image}" >`
+    else
+       var image = ``
+
+    var html = `<div class="message" >
+      <div class="upper-message" >
+        <div class="upper-message__user-name">${message.name}</div>
+        <div class="upper-message__date">${message.strftime}</div>
+      </div>
+      <div class="lowwer-message" >
+        <p class="lower-message__content">${message.content}</p>
+         ${image}
+      </div>
+    </div>`
+  return html;
+  }
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.hidden').val('');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow');
+    })
+    .always(function(){
+      $('input').prop("disabled", false);
+    })
+    .fail(function(){
+      alert('メッセージを入力してください。');
+    })
+  })
+});
